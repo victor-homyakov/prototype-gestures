00dedb27a8d73c5eef5bc1bb99a24440fcef6056
Victor <homyakov@isida.by>
2012-11-27 14:53:24 -0430

    Template for functional test

--- js/gestures.js
+++ js/gestures.js
@@ -2,5 +2,10 @@ Event.supportsTouch = ("ontouchstart" in window);
 
 var Gestures = Class.create({
   DEFAULT_OPTIONS: {
+  },
+  initialize: function(element, options) {
+    options = Object.extend(Object.clone(this.DEFAULT_OPTIONS), options || {});
+    this.element = $(element);
+    this.options = options;
   }
 });
