cc7b6c1dca1b22b7bcafea74179ea39d6fe007f2
Victor <homyakov@isida.by>
2012-12-17 15:27:07 -0430

    Added method stubs

--- js/gestures.js
+++ js/gestures.js
@@ -1,11 +1,79 @@
+/**
+ * Licensed under the MIT license.
+ */
 Event.supportsTouch = ("ontouchstart" in window);
+// TODO Event.supportsTouch = Event.isSupported("ontouchstart", window);
+// Exception: Windows Phone 7.5 does not support touch events
 
 var Gestures = Class.create({
   DEFAULT_OPTIONS: {
   },
+
+  prebind: ["handleTouchStart", "handleTouchMove", "handleTouchEnd", "handleMouseOut"],
+
   initialize: function(element, options) {
     options = Object.extend(Object.clone(this.DEFAULT_OPTIONS), options || {});
     this.element = $(element);
     this.options = options;
+
+    this.pos = {};
+    this.offset = {};
+
+    this.observe();
+  },
+
+  destroy: function() {
+    this.stopObserving();
+  },
+
+  observe: Event.supportsTouch ? function() {
+    // for touch devices
+    var e = this.element;
+    e.observe("touchstart", this.handleTouchStart);
+    e.observe("touchmove", this.handleTouchMove);
+    e.observe("touchend", this.handleTouchEnd);
+    e.observe("touchcancel", this.handleTouchEnd);
+  } : function() {
+    // for non-touch devices
+    var e = this.element;
+    e.observe("mousedown", this.handleTouchStart);
+    e.observe("mousemove", this.handleTouchMove);
+    e.observe("mouseup", this.handleTouchEnd);
+    e.observe("mouseout", this.handleMouseOut);
+  },
+
+  stopObserving: Event.supportsTouch ? function() {
+    var e = this.element;
+    e.stopObserving("touchstart", this.handleTouchStart);
+    e.stopObserving("touchmove", this.handleTouchMove);
+    e.stopObserving("touchend", this.handleTouchEnd);
+    e.stopObserving("touchcancel", this.handleTouchEnd);
+  } : function() {
+    // for non-touch devices
+    var e = this.element;
+    e.stopObserving("mousedown", this.handleTouchStart);
+    e.stopObserving("mousemove", this.handleTouchMove);
+    e.stopObserving("mouseup", this.handleTouchEnd);
+    e.stopObserving("mouseout", this.handleMouseOut);
+  },
+
+  handleTouchStart: function(event) {
+    // TODO
+    console.log("handleTouchStart", event);
+  },
+
+  handleTouchMove: function(event) {
+    // TODO
+    console.log("handleTouchMove", event);
+  },
+
+  handleTouchEnd: function(event) {
+    // TODO
+    console.log("handleTouchEnd", event);
+  },
+
+  handleMouseOut: function(event) {
+    // TODO
+    console.log("handleMouseOut", event);
   }
 });
