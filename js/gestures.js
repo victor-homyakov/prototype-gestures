791c948d623461947b33fa4db4e80839a358f9fa
Victor <homyakov@isida.by>
2012-12-25 15:19:53 -0430

    Internal event system; "hold" gesture

--- js/gestures.js
+++ js/gestures.js
@@ -3,10 +3,20 @@
  */
 Event.supportsTouch = ("ontouchstart" in window);
 // TODO Event.supportsTouch = Event.isSupported("ontouchstart", window);
-// Exception: Windows Phone 7.5 does not support touch events
+// Exceptions:
+//   Windows Phone 7.5 does not support touch events
+//   Android (until 4.X?) does not support multitouch, there is always 1 touch, i.e. no zoom and rotation
 
-var Gestures = Class.create({
+var Gestures = Class.create(Mixin.Observable, {
   DEFAULT_OPTIONS: {
+    // true to prevent default action for touch event
+    // preventDefault=false may lead to strange behavior (default event and gesture simultaneously)
+    preventDefault: false,
+
+    // hold gesture enabled
+    holdEnabled: true,
+    // timeout in seconds to activate gesture
+    holdTimeout: 0.5
   },
 
   prebind: ["handleTouchStart", "handleTouchMove", "handleTouchEnd", "handleMouseOut"],
@@ -19,14 +29,15 @@ var Gestures = Class.create({
     this.pos = {};
     this.offset = {};
 
-    this.observe();
+    this.observeElement();
   },
 
   destroy: function() {
-    this.stopObserving();
+    this.stopObservingElement();
+    this.element = null;
   },
 
-  observe: Event.supportsTouch ? function() {
+  observeElement: Event.supportsTouch ? function() {
     // for touch devices
     var e = this.element;
     e.observe("touchstart", this.handleTouchStart);
@@ -42,7 +53,7 @@ var Gestures = Class.create({
     e.observe("mouseout", this.handleMouseOut);
   },
 
-  stopObserving: Event.supportsTouch ? function() {
+  stopObservingElement: Event.supportsTouch ? function() {
     var e = this.element;
     e.stopObserving("touchstart", this.handleTouchStart);
     e.stopObserving("touchmove", this.handleTouchMove);
@@ -57,14 +68,38 @@ var Gestures = Class.create({
     e.stopObserving("mouseout", this.handleMouseOut);
   },
 
+  /**
+   * Handle touchstart and mousedown events. Fire "hold" gesture.
+   */
   handleTouchStart: function(event) {
-    // TODO
-    console.log("handleTouchStart", event);
+    this.pos.start = this.getEventPos(event);
+    this.touchStartTime = new Date().getTime();
+    this.fingers = this.countFingers(event);
+    this.first = true;
+    this.eventStart = event;
+
+    /*
+     var offset = this.element.viewportOffset(), scroll = document.viewport.getScrollOffsets();
+     this.offset = {
+     top: offset.top + scroll.y,
+     left: offset.left + scroll.x
+     };
+     */
+    this.offset = Element.cumulativeOffset(this.element);
+    this.mousedown = true;
+
+    console.log("handleTouchStart", event, this.pos.start, this.offset);
+    // detect "hold" gesture
+    this.detectHold(event);
+
+    if (this.options.preventDefault) {
+      Event.stop(event);
+    }
   },
 
   handleTouchMove: function(event) {
     // TODO
-    console.log("handleTouchMove", event);
+    //console.log("handleTouchMove", event);
   },
 
   handleTouchEnd: function(event) {
@@ -73,7 +108,76 @@ var Gestures = Class.create({
   },
 
   handleMouseOut: function(event) {
-    // TODO
     console.log("handleMouseOut", event);
+    // event.relatedTarget - W3C, event.toElement - IE
+    // Prototype provides unified event.relatedTarget
+    if (!event.relatedTarget || !Element.descendantOf(event.relatedTarget, this.element)) {
+      this.handleTouchEnd(event);
+    }
+  },
+
+  // Event details
+
+  /**
+   * Get coordinates of each touch in event.
+   * @param {Event} event
+   * @return {Array} of points (int x, int y)
+   */
+  getEventPos: Event.supportsTouch ? function(event) {
+    // multitouch, return array with positions
+    var pos = [];
+    for (var t = 0, len = event.touches.length; t < len; t++) {
+      var src = event.touches[t];
+      pos.push({
+        x: src.pageX,
+        y: src.pageY
+      });
+    }
+    return pos;
+  } : function(event) {
+    // no touches, use the event pageX and pageY
+    return [{
+      x: event.pageX,
+      y: event.pageY
+    }];
+  },
+
+  /**
+   * Count the number of fingers (touches) in the event.
+   * When no fingers are detected, one finger is returned (mouse pointer).
+   * @param {Event} event
+   * @return {Number} fingers
+   */
+  countFingers: function(event) {
+    // Android (until v4?) does not support event.touches
+    return event.touches ? event.touches.length : 1;
+  },
+
+  // Detect and fire gestures
+
+  notifyGesture: function(eventName, params) {
+    params.touches = this.getEventPos(params.originalEvent);
+    params.type = eventName;
+    this.notify(eventName, params);
+  },
+
+  /**
+   * Hold gesture detection - fired by handleTouchStart.
+   */
+  detectHold: function(event) {
+    // TODO
+    // only when one finger is on the screen
+    if (this.options.holdEnabled) {
+      this.gesture = "hold";
+      clearTimeout(this.holdTimer);
+      this.holdTimer = (function(event, pos) {
+        if (this.gesture === "hold") {
+          this.notifyGesture("hold", {
+            originalEvent: event,
+            position: pos.start
+          });
+        }
+      }).bind(this, event, this.pos).delay(this.options.holdTimeout);
+    }
   }
 });
